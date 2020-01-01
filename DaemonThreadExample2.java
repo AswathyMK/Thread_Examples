41afdfa9c569be19887ba68b09a0eaa88a01a71e
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-01 14:43:38 +0530

    Program using DaemonThread.

--- DaemonThreadExample2.java
+++ DaemonThreadExample2.java
@@ -1,18 +1,21 @@
 
-public class DaemonThreadExample2 extends Thread {
+public class DaemonThreadExample2 extends Thread 
+{
 	public void run()
 	{
-	System.out.println("Thread name :"+Thread.currentThread().getName());
-	System.out.println("Check if its DaemonThread:"+Thread.currentThread().getName());
+	System.out.println("Thread name :"+Thread.currentThread().getName());//return the name of current thread
+        System.out.println("Check if its DaemonThread:"+Thread.currentThread().isDaemon());//checks the thread is Daemon Thread or not.
+	
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DaemonThreadExample2 t1=new DaemonThreadExample2();
-		DaemonThreadExample2 t2=new DaemonThreadExample2();
-		t1.start();
+	public static void main(String[] args) 
+	{
+		DaemonThreadExample2 t1=new DaemonThreadExample2();//Creating object for Daemonthread
+		DaemonThreadExample2 t2=new DaemonThreadExample2();//Creating object for user thread
+		//t1.start();//Here it makes exception
 		//Exception at the Thread is already started.
 		t1.setDaemon(true);
+		t1.start();//to avoid exception start after setDaemon()
 		t2.start();
 
 	}
